fa5ab8178ffd577f1ffb8536804fe78f5c5460b7
hirofumi2012 <h209022@gmail.com>
2018-02-24 21:49:44 +0900

    add factorize.rb

---
+++ factorize.rb
@@ -0,0 +1,15 @@
+# coding: SJIS
+
+def factorize(n)
+  i = 2
+  while(i ** 2 <= n)
+    return [i] + factorize(n / i) if(n % i == 0)
+    i += 1
+  end
+  return [n]
+end
+
+print "‘fˆö”•ª‰ð‚·‚é”F"
+num = gets.to_i
+
+p factorize(num)
